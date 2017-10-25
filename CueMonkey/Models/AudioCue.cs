ffacd7eebcea08c6264437a14c5834fbf9fe7441
Ben Dow <marsmandow@gmail.com>
2017-10-25 01:15:43 -0400

    Continuted UI Design and added models for some cues

---
+++ CueMonkey/Models/AudioCue.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CueMonkey.Models
+{
+
+    class AudioCue : Cue
+    {
+        public string Source { get; set; }
+        public TimeSpan Length { get; set; }
+
+
+    }
+}
--- CueMonkey/Models/Cue.cs
+++ CueMonkey/Models/Cue.cs
@@ -6,12 +6,28 @@ using System.Threading.Tasks;
 
 namespace CueMonkey.Models
 {
+
     class Cue
     {
         public string Name { get; set; }
         public double Number { get; set; }
         public string Notes { get; set; }
-
         public Cue Target { get; set; }
+
+        public Cue() {
+
+        }
+
+        public Cue(string Name) {
+            this.Name = Name;
+        }
+
+        public Cue(string Name, double Number, string Notes, Cue Target) {
+            this.Name = Name;
+            this.Number = Number;
+            this.Notes = Notes;
+            this.Target = Target;
+        }
+
     }
 }
